755ca0490599e48cc7d9c0d921ab80ad978aaf84
BoujeeWhoCodes <imellettis28@gmail.com>
2024-04-12 18:27:55 +0800

    Added github repo URL

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import './App.css';
 export default function App() {
 	return(
     <div className="App">
-	  Weather App	
+	  <h1>Weather App</h1>
 	  <footer>
-		This project was coded by Bougie and 
+		This project was coded by < a href="">Bougie</a> 
+		and is {""}
     
 	  <a href="https://github.com/BoujeeWhoCodes/week-5-app"
 	  target="_blank">	
-	  
+
      open-sourced on Github
 	  </a>
 	  </footer>
