4ab748159a4d87c8822e15a41f13e91874d215ed
BoujeeWhoCodes <imellettis28@gmail.com>
2024-04-12 18:22:56 +0800

    Added all necessary stuff

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ import './App.css';
 export default function App() {
 	return(
     <div className="App">
-      Weather App
+	  Weather App	
+	  <footer>
+		This project was coded by Bougie and 
+    
+	  <a href="https://github.com/BoujeeWhoCodes/week-5-app"
+	  target="_blank">	
+	  
+     open-sourced on Github
+	  </a>
+	  </footer>
 		</div>
 	);
 }
