366178e983e819f1a20948078065517ca855b576
BoujeeWhoCodes <imellettis28@gmail.com>
2024-04-15 16:47:53 +0800

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -7,18 +7,19 @@ export default function Weather() {
 		<div className='Weather'>
 			<form>
 				<div className='row'>
-                    <div className="col-9">
-					<input
-						type='search'
-						placeholder='Enter a city ... '
-						className='form-control'
-					/>
-                    </div>
-					<div className="col-3">
+					<div className='col-9'>
+						<input
+							type='search'
+							placeholder='Enter a city ... '
+							className='form-control'
+							autoFocus='on'
+						/>
+					</div>
+					<div className='col-3'>
 						<input
 							type='Submit'
 							value='Search'
-							className='btn btn-primary'
+							className='btn btn-primary w-100'
 						/>
 					</div>
 				</div>
@@ -28,19 +29,21 @@ export default function Weather() {
 				<li>Monday 03:00</li>
 				<li>Mostly Cloudy</li>
 			</ul>
-			<div className='row'>
+			<div className='row mt-3'>
 				<div className='col-6'>
 					<img
 						src='https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png'
 						alt='Mostly Cloudy'
+						className='float-left'
 					/>
-					16°C
+					<span className='temperature'>16</span>
+					<span className='unit'>°C</span>
 				</div>
 				<div className='col-6'>
 					<ul>
 						<li>Precipitation: 4%</li>
 						<li>Humidity : 74%</li>
-						<li>Wind :6 km/h</li>
+						<li>Wind : 6 km/h</li>
 					</ul>
 				</div>
 			</div>
