641ae072d84e64bf642b45e580bdda5120387f9b
BoujeeWhoCodes <imellettis28@gmail.com>
2024-04-19 16:39:06 +0800

    Added axios

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ export default function App() {
 	return(
     <div className="App">
 		<div className="container">
-	
-	  <h1>Weather App</h1>
-	  <Weather/>
+	<Weather defaultCity= "New York"/>
+	  
 	  <footer>
 		This project was coded by < a href="">Bougie</a> 
 		{""} and is {""}
