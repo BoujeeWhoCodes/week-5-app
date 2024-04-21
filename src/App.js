9dbea55306472096f94449634e5e1b62f20e0ba2
BoujeeWhoCodes <imellettis28@gmail.com>
2024-04-21 21:45:08 +0800

    corrected typo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-/** @format */
 
 import React from 'react';
-import Weather from "./Weather";
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
-	return(
-    <div className="App">
-		<div className="container">
-	<Weather defaultCity= "New York"/>
-	  
-	  <footer>
-		This project was coded by < a href="">Bougie</a> 
-		{""} and is {""}
-    
-	  <a href="https://github.com/BoujeeWhoCodes/week-5-app"
-	  target="_blank">	
-
-     open-sourced on Github
-	  </a>
-	  </footer>
-	  </div>
+	return (
+		<div className='App'>
+			<div className='container'>
+				<Weather defaultCity='New York' />
+				<footer>
+					This project was coded by <a href='/'>Bougie</a> and is{' '}
+					<a
+						href='https://github.com/BoujeeWhoCodes/week-5-app'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						open-sourced on Github
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
